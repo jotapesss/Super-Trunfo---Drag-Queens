e830703364558a48db8870e98f47ac1cca029389
João Pedro Schmidt <101834573+jotapesss@users.noreply.github.com>
2022-03-18 18:26:44 -0300

    Adicionei os arquivos do Super Trunfo

---
+++ script.js
@@ -0,0 +1,279 @@
+var carta1 = {
+  nome: "Bebe Zahara Benet",
+  imagem: "http://pm1.narvii.com/6935/7eb761e8c950ff5b1159b36623d41de0dab54f02r1-750-927v2_uhq.jpg",
+  atributos: {
+    Episodes: 17,
+    Victories: 4,
+    Score: 747
+  }
+};
+
+var carta2 = {
+  nome: "Tyra Sanchez",
+  imagem: "https://i0.wp.com/draglicious.com.br/wp-content/uploads/2020/01/tyra-sanchez-draglicious-cover-header-capa-2020.jpg",
+  atributos: {
+    Episodes: 12,
+    Victories: 3,
+    Score: 840
+  }
+};
+
+var carta3 = {
+  nome: "Kylie Sonique Love",
+  imagem: "https://pbs.twimg.com/profile_images/1455766741527629824/sC6IDqsz_400x400.jpg",
+  atributos: {
+    Episodes: 16,
+    Victories: 1,
+    Score: 693
+  }
+};
+
+var carta4 = {
+  nome: "Raja",
+  imagem: "https://pbs.twimg.com/media/DROnl-NW4AAwsWe.jpg",
+  atributos: {
+    Episodes: 16,
+    Victories: 3,
+    Score: 800
+  }
+};
+
+var carta5 = {
+  nome: "Sharon Needles",
+  imagem: "https://lastfm.freetls.fastly.net/i/u/770x0/5fe2d7810c19d6e10a5699de190dfbe6.jpg",
+  atributos: {
+    Episodes: 14,
+    Victories: 4,
+    Score: 825
+  }
+};
+
+var carta6 = {
+  nome: "Chad Michaels",
+  imagem: "https://i.pinimg.com/736x/03/c2/f3/03c2f3467e3f4aa6178f2abeb1193fd0--rupauls-dragrace-drag-makeup.jpg",
+  atributos: {
+    Episodes: 20,
+    Victories: 5,
+    Score: 794
+  }
+};
+
+var carta7 = {
+  nome: "Jinkx Monsoon",
+  imagem: "https://cdns-images.dzcdn.net/images/artist/430cecca2c9a85f1ec6b624716e8f04e/500x500.jpg",
+  atributos: {
+    Episodes: 14,
+    Victories: 2,
+    Score: 808
+  }
+};
+
+var carta8 = {
+  nome: "Alaska",
+  imagem: "https://alaskathunderfuck.com/wp-content/uploads/2021/03/alaska-bio-alaska-image-3.jpg",
+  atributos: {
+    Episodes: 23,
+    Victories: 6,
+    Score: 800
+  }
+};
+
+var carta9 = {
+  nome: "Bianca del Rio",
+  imagem: "https://static.onecms.io/wp-content/uploads/sites/6/2016/10/bianca-del-rio-not-today-bianca-bat_7781.jpg",
+  atributos: {
+    Episodes: 14,
+    Victories: 3,
+    Score: 842
+  }
+};
+
+var carta10 = {
+  nome: "Violet Chachki",
+  imagem: "https://64.media.tumblr.com/59564def5455f3745fdabc55954f9f93/tumblr_pobrawtlos1vdp8e6_1280.jpg",
+  atributos: {
+    Episodes: 14,
+    Victories: 3,
+    Score: 785
+  }
+};
+
+var carta11 = {
+  nome: "Trixie Mattel",
+  imagem: "https://i.pinimg.com/originals/fa/08/9f/fa089f4acc39b65e0974eebd94cf3390.jpg",
+  atributos: {
+    Episodes: 15,
+    Victories: 3,
+    Score: 707
+  }
+};
+
+var carta12 = {
+  nome: "Bob the Drag Queen",
+  imagem: "https://preview.redd.it/rnuad3fxqyf41.jpg?auto=webp&s=59d003f727dc65cb7cdda772c7e5ce8d96e3c387",
+  atributos: {
+    Episodes: 10,
+    Victories: 3,
+    Score: 800
+  }
+};
+
+var carta13 = {
+  nome: "Sasha Velour",
+  imagem: "https://flxt.tmsimg.com/assets/p18147392_i_v10_aa.jpg",
+  atributos: {
+    Episodes: 14,
+    Victories: 2,
+    Score: 800
+  }
+};
+
+var carta14 = {
+  nome: "Shea Couleé",
+  imagem: "https://anotherimg-dazedgroup.netdna-ssl.com/1600/azure/another-prod/400/0/400315.jpg",
+  atributos: {
+    Episodes: 22,
+    Victories: 6,
+    Score: 771
+  }
+};
+
+var carta15 = {
+  nome: "Trinity the Tuck",
+  imagem: "https://i.pinimg.com/originals/01/b1/33/01b1335712704563d1a2b70efc5b7bc7.jpg",
+  atributos: {
+    Episodes: 24,
+    Victories: 7,
+    Score: 765
+  }
+};
+
+var carta16 = {
+  nome: "Aquaria",
+  imagem: "https://i.pinimg.com/originals/cb/8e/97/cb8e97ac17793d58c5a000a76d79be0d.jpg",
+  atributos: {
+    Episodes: 14,
+    Victories: 3,
+    Score: 785
+  }
+};
+
+var carta17 = {
+  nome: "Monét X Change",
+  imagem: "https://www.monstersandcritics.com/wp-content/uploads/2018/12/Monet-X-Change.jpg",
+  atributos: {
+    Episodes: 20,
+    Victories: 3,
+    Score: 715
+  }
+};
+
+var carta18 = {
+  nome: "Yvie Oddly",
+  imagem: "https://i0.wp.com/imageamplified.com/wp-content/uploads/2019/08/ATTITUDE-UK-Yvie-Oddly-by-Brian-Jamie.-August-2019-www.imageamplified.com-Image-Amplified7_thumb.jpg",
+  atributos: {
+    Episodes: 14,
+    Victories: 1,
+    Score: 769
+  }
+};
+
+var carta19 = {
+  nome: "Jaida Essence Hall",
+  imagem: "https://pbs.twimg.com/media/ER0J7CzXYAAT3z-.jpg",
+  atributos: {
+    Episodes: 14,
+    Victories: 3,
+    Score: 808
+  }
+};
+
+var carta20 = {
+  nome: "Symone",
+  imagem: "https://assets.vogue.com/photos/608723ff7c9137c60295c7e9/3:4/w_606,h_808,c_limit/175323558_4590489752352458_23893839380170380_n.jpeg",
+  atributos: {
+    Episodes: 16,
+    Victories: 4,
+    Score: 800
+  }
+};
+
+
+var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18, carta19, carta20];
+var cartaMaquina;
+var cartaJogador;
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * cartas.length);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * cartas.length);
+  while (numeroCartaJogador == numeroCartaMaquina) {
+    numeroCartaJogador = parseInt(Math.random() * cartas.length);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+  var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+  var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+
+  if (valorCartaJogador > valorCartaMaquina) {
+    htmlResultado = "<p class='resultado-final'>Você venceu!</p>"
+  } else if (valorCartaMaquina > valorCartaJogador) {
+    htmlResultado = "<p class='resultado-final'>Você perdeu!</p>"
+  } else {
+    htmlResultado = "<p class='resultado-final'>Empatou!</p>"
+  }
+divResultado.innerHTML = htmlResultado;
+document.getElementById('btnJogar').disabled = true;
+exibirCartaMaquina()  
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador")
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+  var moldura ='<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>"
+  
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'checked>" + atributo + ": " + cartaJogador.atributos[atributo] + "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+  
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + '</div>'
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina")
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+  var moldura ='<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>"
+  
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + ": " + cartaMaquina.atributos[atributo] + "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+  
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + '</div>'
+}
